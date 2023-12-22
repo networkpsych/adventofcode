c0413f29db2bfd37364becec0724c42c6f97697d
Autoclitic <braydenchristensen6@gmail.com>
2023-12-21 21:13:05 -0700

    modified:   day14/day14.py         Cleaned up and fixed issues.

--- day14/day14.py
+++ day14/day14.py
@@ -3,8 +3,8 @@ O == rounded rock
 # == square rock
 . == open space
 
-If the place of (row, col) contains a O or a # 
-    Check (row, col) around to see if there is an empty space
+If the place of (row, item) contains a O or a # 
+    Check (row, item) around to see if there is an empty space
         If there is an empty space -> move rock
         Otherwise -> rock stays in place
 
@@ -16,95 +16,128 @@ EXAMPLE
 
 
 """
+
+
 def part_one():
-    file = open('input.txt', 'r')
-    x = [list(r.strip()) for r in file.readlines()]
-    print(x)
+    file = open("input.txt", "r")
+    beams = [list(r.strip()) for r in file.readlines()]
+    print(beams)
 
     weight = 0
-    for idx, row in enumerate(x):
-        for cidx in range(len(row)):
-            pos = x[idx][cidx] # get the current position
+    for idx, beam in enumerate(beams):
+        for col in range(len(beam)):
+            pos = beams[idx][col]  # get the current position
             if pos == "O":
-                x[idx][cidx] = "." # set the index to be empty
-                tilt = idx - 1 # tilt the platform
+                beams[idx][col] = "."  # set the index to be empty
+                tilt = idx - 1  # tilt the platform
                 # while the platform is tilted and the index is empty
-                while tilt >= 0 and x[tilt][cidx] == ".":
+                while tilt >= 0 and beams[tilt][col] == ".":
                     # subtract the position
                     tilt -= 1
                 # when the position breaks
-                x[tilt+1][cidx] = "O" # change the previous row to a stone
-                weight += len(x) - tilt - 1 # add the weight
-        print(x[idx])
+                beams[tilt + 1][col] = "O"  # change the previous row to a stone
+                weight += len(beams) - tilt - 1  # add the weight
+        print(beams[idx])
 
     print(weight)
 
+
 def part_two():
-    file = open('input.txt', 'r')
-    x = [list(r.strip()) for r in file.readlines()]
+    file = open("input.txt", "r")
+    beams = [list(r.strip()) for r in file.readlines()]
     d, cycles, states = 0, 0, {}
 
     while True:
         if d in {0, 2}:
             if d == 2:
-                x.reverse()
+                beams.reverse()
             else:
                 if states is not None:
-                    _new = tuple([(ridx, cidx) for ridx, row in enumerate(x) for cidx, col in enumerate(row) if col == "O"])
+                    _new = tuple(
+                        [
+                            (row, col)
+                            for row, beam in enumerate(beams)
+                            for col, item in enumerate(beam)
+                            if item == "O"
+                        ]
+                    )
                     if _new in states:
-                        cycles = 1_000_000_000 - (1_000_000_000 - cycles) % (cycles - states[_new])
+                        # print(1000000000 - (1000000000 - cycles) % (cycles - states[_new]))
+                        cycles = 1000000000 - (1000000000 - cycles) % (
+                            cycles - states[_new]
+                        )
                         states = None
                     else:
                         states[_new] = cycles
-            for idx, row in enumerate(x):
-                for cidx in range(len(row)):
-                    pos = x[idx][cidx] # get the current position
+            for idx, beam in enumerate(beams):
+                for col in range(len(beam)):
+                    pos = beams[idx][col]  # get the current position
                     if pos == "O":
-                        x[idx][cidx] = "." # set the index to be empty
-                        tilt = idx - 1 # tilt the platform
+                        beams[idx][col] = "."  # set the index to be empty
+                        tilt = idx - 1  # tilt the platform
                         # while the platform is tilted and the index is empty
-                        while tilt >= 0 and x[tilt][cidx] == ".":
+                        while tilt >= 0 and beams[tilt][col] == ".":
                             # subtract the position
                             tilt -= 1
                         # when the position breaks
-                        x[tilt+1][cidx] = "O" # change the previous row to a stone
-            
+                        beams[tilt + 1][col] = "O"  # change the previous row to a stone
+
             if d == 2:
-                x.reverse()
+                beams.reverse()
+
+        # process the east & west tilting
         elif d == 1:
-            for idx, row in enumerate(x[0]):
-                for cidx in range(len(row)):
-                    pos = x[idx][cidx] # get the current position
+            for col in range(len(beams[0])):
+                for idx in range(len(beams)):
+                    pos = beams[idx][col]  # get the current position
                     if pos == "O":
-                        x[idx][cidx] = "." # set the index to be empty
-                        tilt = cidx - 1
-                        while tilt >= 0 and x[idx][tilt] == ".":
+                        beams[idx][col] = "."  # set the index to be empty
+                        tilt = col - 1
+                        while tilt >= 0 and beams[idx][tilt] == ".":
                             tilt -= 1
-                    # when the position breaks
-                    x[idx][tilt+1] = "O" # change the previous row to a stone  
+                        # when the position breaks
+                        beams[idx][tilt + 1] = "O"  # change the previous row to a stone
         else:
-            for idx in range(len(x[0]) - 1, -1, -1):
-                for cidx in range(len(x)):
-                    pos = x[idx][cidx] # get the current position
+            for col in range(len(beams[0]) - 1, -1, -1):
+                for idx in range(len(beams)):
+                    pos = beams[idx][col]  # get the current position
                     if pos == "O":
-                        x[idx][cidx] = "." # set the index to be empty
-                        tilt = cidx + 1
-                        while tilt < len(x[0]) and x[idx][tilt] == ".":
+                        beams[idx][col] = "."  # set the index to be empty
+                        tilt = col + 1
+                        while tilt < len(beams[0]) and beams[idx][tilt] == ".":
                             tilt += 1
-                        x[idx][tilt-1] = "O"
+                        beams[idx][tilt - 1] = "O"
             cycles += 1
             if cycles == 1_000_000_000:
-                print(sum([len(x) - ridx for ridx, row in enumerate(x) for cidx, col in enumerate(row) if col == "O"]))
+                print(
+                    sum(
+                        [
+                            len(beams) - row
+                            for row, beam in enumerate(beams)
+                            for col, item in enumerate(beam)
+                            if item == "O"
+                        ]
+                    )
+                )
                 quit()
         d = (d + 1) % 4
+        # print(cycles, end='\r')
 
 
 def example():
-    file = open('example.txt', 'r')
-    x = [list(r.strip()) for r in file.readlines()]
-    _new = tuple([(cidx, ridx) for ridx, row in enumerate(x) for cidx, col in enumerate(row) if col == "O"])
-    print(x)
+    file = open("example.txt", "r")
+    beams = [list(r.strip()) for r in file.readlines()]
+    _new = tuple(
+        [
+            (col, row)
+            for row, beam in enumerate(beams)
+            for col, item in enumerate(beam)
+            if item == "O"
+        ]
+    )
+    print(beams)
     print(_new)
-    
-example()
+
+
+# example()
 part_two()
